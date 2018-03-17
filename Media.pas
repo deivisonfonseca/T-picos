3a7c53b47f102bba8460062f5670d44b1e584ae9
João Eduardo Vasconcellos da Silva <joao.eduardovsilva@hotmail.com>
2018-03-16 22:05:04 -0300

    O arquivo foi criado

---
+++ Media.pas
@@ -0,0 +1,9 @@
+Program Pzim ;
+var nota1,nota2,m:real;
+Begin
+       writeln('Digite suas notas');
+       readln(nota1,nota2);
+       m:=(nota1+nota2)/2;
+       writeln('Média = ',m:0:1);
+       
+End.
