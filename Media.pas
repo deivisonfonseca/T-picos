8e849b5eb0c7922fc6a2aac2b84b7fed60461617
deivisonfonseca <deivisonnascimento06@gmail.com>
2018-03-16 22:16:25 -0300

    Please enter the commit message for your changes. Lines starting

--- Media.pas
+++ Media.pas
@@ -3,7 +3,7 @@ var nota1,nota2,m:real;
 Begin
        writeln('Digite suas notas');
        readln(nota1,nota2);
-       m:=(nota1+nota2)/2;
+       m:=(nota1+nota2)/4;
        writeln('Média = ',m:0:1);
        
 End.
